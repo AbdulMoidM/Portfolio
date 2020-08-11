af958ec999e8d0f249b167bce140f6933dcf6fff
AbdulMoidM <53446831+AbdulMoidM@users.noreply.github.com>
2020-08-11 06:13:03 -0400

    Updates

---
+++ precache-manifest.07a9b0d767dffa0f9de7d131d8296d41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3743a502d4e74a1b881b53790c7e527d",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "fd034f23600657f6fcf5",
+    "url": "/My-Portfolio/static/css/main.bc27793a.chunk.css"
+  },
+  {
+    "revision": "ef32f7f0c459ab76ae13",
+    "url": "/My-Portfolio/static/js/2.b45bf8a4.chunk.js"
+  },
+  {
+    "revision": "01a8dbd95aeda551f59a02b8fe4331ab",
+    "url": "/My-Portfolio/static/js/2.b45bf8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd034f23600657f6fcf5",
+    "url": "/My-Portfolio/static/js/main.bf4fe4a8.chunk.js"
+  },
+  {
+    "revision": "e27ee06a52d2e1ce7e8f",
+    "url": "/My-Portfolio/static/js/runtime-main.2f36d5cc.js"
+  },
+  {
+    "revision": "8aad82525b6f45cca65e604f37cbb137",
+    "url": "/My-Portfolio/static/media/Lstm.8aad8252.PNG"
+  },
+  {
+    "revision": "16ad4cf1e2f68e5fa52938f25287da44",
+    "url": "/My-Portfolio/static/media/headshot.16ad4cf1.jpg"
+  },
+  {
+    "revision": "4d09ba5f24c54b2a88d5e45ac8f9f263",
+    "url": "/My-Portfolio/static/media/shortest-path-example.4d09ba5f.PNG"
+  }
+]);
