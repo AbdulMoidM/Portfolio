5ec1ed9b86fe0489edcbc54f479e4ea08f8d2941
AbdulMoidM <53446831+AbdulMoidM@users.noreply.github.com>
2020-08-11 08:36:46 -0400

    'scrollfix'

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -8,30 +8,16 @@ function TopSection() {
 
   return (
 
-          <div className="masthead bg-primary text-white text-center mb-0 pb-0">
-              <div className="container d-flex align-items-center flex-column">
-                <img className="masthead-avatar mb-5" src={headshot} alt="my headshot" style={{height:'400px', width:'400px', borderRadius:'75%'}} />
+          <div  style={{paddingTop:'7rem'}}className="masthead bg-primary text-white text-center mb-0 pb-0">
+              <div style={{margin:'0px',padding:'0px'}} className=" d-flex align-items-center flex-column">
+                <img className="masthead-avatar mb-3" src={headshot} alt="my headshot" style={{height:'19rem', width:'19rem', borderRadius:'75%'}} />
 
-                  <h1 className="masthead-heading mb-0">Abdul Moid Munawar</h1>
+                  <h1 style={{fontSize:'250%'}} className="masthead-heading mb-0">Abdul Moid Munawar</h1>
                   {// Icon Divider-->
                   }
                   <div className="divider-custom divider-light">
-                      <div className="divider-custom-line"></div>
                       <div className="divider-custom-icon">
-                              <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-star-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                              </svg>
-                      </div>
-                      <div className="divider-custom-line"></div>
-                  </div>
-                  {// Masthead Subheading-->
-                  }
-                  <p className="pre-wrap masthead-subheading font-weight-light mb-0">Full Stack Web Developer,
-          Junior Undergraduate, University of Rochester
-          Majoring in Computer Science and Economics</p>
-              </div>
-
-                      <div className="row mt-5">
+                      <div className="row mt-2">
                           <div className="col-lg-12 mb-5 mb-lg-0">
                               <a className="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/a.moid10" target="_blank"><i className="fa fa-facebook"></i></a>
                               <a className="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/AbdulMoidM/" target="_blank"><i className="	fa fa-linkedin"></i></a>
@@ -47,6 +33,16 @@ function TopSection() {
                           }
 
                       </div>
+                      </div>
+                  </div>
+                  {// Masthead Subheading-->
+                  }
+                  <p className="pre-wrap masthead-subheading font-weight-light mb-0">Full Stack Web Developer,
+          Junior Undergraduate, University of Rochester
+          Majoring in Computer Science and Economics</p>
+              </div>
+
+
           </div>
 
         );
